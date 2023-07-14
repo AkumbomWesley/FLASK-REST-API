f2341151bf83fcfa5b01fe007db2df12156ec787
weskeyz <keyzwesley@gmail.com>
2023-07-14 12:56:42 +0100

    RESTFUL

--- restful.py
+++ restful.py
@@ -5,5 +5,5 @@ app = Flask(__name__)
 def teset():
     return jsonify({'message':'It works!'})
 
-if __name__ == '__main__':
-    app.run(debug=True, port=8080) #run app on port 8080 in debug mode
+#if __name__ == '__main__':
+    
